8fadd01323c3cf9fc1d6494e023dd860409214ca
Karun Dixit <karundixit5@gmail.com>
2023-06-25 12:54:45 +0545

    Work till now

---
+++ src/database/ConnectionProvider.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Karun Dixit
+ */
+public class ConnectionProvider {
+
+    public static Connection getCon() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
